a2c1f5e588dd3f2134413c13473f10b65ab8b7b3
yxhpy <1764807112@qq.com>
2024-04-16 09:53:33 +0800

    #新增 支持的配置清单展示

--- config/config.py
+++ config/config.py
@@ -3,11 +3,62 @@ import os
 HOME_PATH = os.path.dirname(os.path.dirname(__file__))
 TOKEN_CONFIG = [
     {
+        "token": "sk-1",
+        "type": "zhipu",
+        "config": {"api_key": ""}
+    },
+    {
+        "token": "sk-2",
+        "type": "spark",
+        "config": {
+            "appid": "",
+            "api_secret": "",
+            "api_key": "",
+            "gpt_url": "",
+            "domain": ""
+        },
+    },
+    {
+        "token": "sk-3",
+        "type": "maxai",
+        "config": {
+            "api_key": "",
+        },
+    },
+    {
+        "token": "sk-4",
+        "type": "claude",
+        "config": {
+            "api_key": ""
+        }, },
+    {
+        "token": "sk-5",
+        "type": "geminiweb",
+        "config": {
+            "Secure_1PSID": "",
+            "Secure_1PSIDTS": ""
+        },
+    },
+    {
         "token": "sk-6",
         "type": "geminillm",
         "config": {
-            "api_key": "AIzaSyDQptDlelJqMbOxnd6yjbzgzsSJW0bSd-s"
+            "api_key": ""
         },
     },
-
+    {
+        "token": "sk-7",
+        "type": "kimi",
+        "config": {
+            "api_key": ""
+        },
+    },
+    {
+        "token": "sk-8",
+        "type": "bedrock",
+        "config": {
+            "token": "",
+            "csrf_token": "",
+        },
+    },
 ]
