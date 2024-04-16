4aa8a26a7605c4f39703ce3473d0ba756f7c8b05
yxhpy <1764807112@qq.com>
2024-04-16 09:50:09 +0800

    #修复 删除token重置提交

---
+++ config/config.py
@@ -0,0 +1,13 @@
+import os
+
+HOME_PATH = os.path.dirname(os.path.dirname(__file__))
+TOKEN_CONFIG = [
+    {
+        "token": "sk-6",
+        "type": "geminillm",
+        "config": {
+            "api_key": "AIzaSyDQptDlelJqMbOxnd6yjbzgzsSJW0bSd-s"
+        },
+    },
+
+]
